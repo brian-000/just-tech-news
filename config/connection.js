6cb763400d0067f8e93015b3f186f24ca6724190
Brain <ortiz_brian323@yahoo.com>
2022-05-16 15:37:13 -0700

    connect to jawsdb

--- config/connection.js
+++ config/connection.js
@@ -1,3 +1,8 @@
+const Sequelize = require('sequelize');
+
+require('dotenv').config();
+
+// create connection to our db
 let sequelize;
 
 if (process.env.JAWSDB_URL) {
@@ -9,3 +14,4 @@ if (process.env.JAWSDB_URL) {
     port: 3306
   });
 }
+module.exports = sequelize;
